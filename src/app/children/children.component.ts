e40d7e71f79503fe9f6524580014f910aa1fc2df
william.luna <william.luna@rappi.com>
2023-12-17 13:15:40 -0500

    21 - @Output (comunicación entre componentes)

--- src/app/children/children.component.ts
+++ src/app/children/children.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output , EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-children',
@@ -7,4 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class ChildrenComponent {
   @Input() title?: string;
+
+  @Output() titleChange = new EventEmitter<string>();
+
+  emitTitleChange() {
+    this.titleChange.emit(this.title);
+  }
 }
